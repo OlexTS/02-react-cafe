ccd81e27568670c2276adc7ccb4d1fb37c15a790
Olexandr <sashokseven@gmail.com>
2025-07-02 23:55:11 +0300

    add state to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "./CafeInfo/CafeInfo";
+import type { Votes, VoteType } from "../types/votes";
 
 function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes({ ...votes, [type]: votes[type] + 1 });
+  };
+
+  const resetVotes = () => {
+    setVotes({ ...votes, good: 0, neutral: 0, bad: 0 });
+  };
   return (
     <div className={css.app}>
       <CafeInfo />
